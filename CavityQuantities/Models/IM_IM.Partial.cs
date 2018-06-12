e2dcc6a7bbaa3b74e4505086c8f56a21c7d8a60c
mhildner <mike.hildner@sunhydraulics.com>
2018-06-12 16:39:25 -0400

    Initial app.

---
+++ CavityQuantities/Models/IM_IM.Partial.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CavityQuantities.Models
+{
+    partial class IM_IM
+    {
+        public string Family4
+        {
+            get
+            {
+                //Debug.WriteLine($"Part_Number: {Part_Number}");
+
+                if (Family == null || Family.Length < 4)
+                {
+                    if(Long_Item_Nbr == null || Long_Item_Nbr.Length < 4)
+                    {
+                        return Long_Item_Nbr;
+                    }
+
+                    return Long_Item_Nbr.Substring(0, 4);
+                }
+                else
+                {
+                    if (Family.Length < 4)
+                    {
+                        return Family;
+                    }
+
+                    return Family.Substring(0, 4);
+                }
+            }
+        }
+
+        public string PartNbr
+        {
+            get { return Part_Number; }
+        }
+
+        public string Family5
+        {
+            get
+            {
+                //Debug.WriteLine($"Part_Number: {Part_Number}");
+
+                if (Family == null || Family.Length < 5)
+                {
+                    if (Long_Item_Nbr == null || Long_Item_Nbr.Length < 5)
+                    {
+                        return Long_Item_Nbr;
+                    }
+
+                    return Long_Item_Nbr.Substring(0, 5);
+                }
+                else
+                {
+                    if (Family.Length < 5)
+                    {
+                        return Family;
+                    }
+
+                    return Family.Substring(0, 5);
+                }
+            }
+        }
+    }
+}
--- CavityQuantities/Program.cs
+++ CavityQuantities/Program.cs
@@ -1,15 +1,121 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CavityQuantities.Models;
+using System.Diagnostics;
 
 namespace CavityQuantities
 {
     class Program
     {
+        private static M2K_DATA_WHSEEntities _M2KDb = new M2K_DATA_WHSEEntities();
+        private static List<IM_IM> _ims;
+        private static List<IM_IM> _ims4;
+        private static List<IM_IM> _ims5;
+        private static List<IM_IM> _ims6;
+
         static void Main(string[] args)
         {
+            BuildFamilyLists();
+
+            ReadExcel();
+        }
+
+        private static void BuildFamilyLists()
+        {
+            _ims = _M2KDb.IM_IM.Where(i => i.Fac_Code != "02" && (i.Inventory_Type == "CT" || i.Inventory_Type == "CL")).ToList();
+        }
+
+        private static void ReadExcel()
+        {
+            string fileName = @"e:\Source\Repos\CavityQuantities\CavityQuantities\InputFiles\Models-Series.xlsx";
+            using (var wb = new XLWorkbook(fileName))
+            using (var ws = wb.Worksheet(1))
+            {
+                int rowLocation = 0;
+
+                // 5 letter model codes.
+                foreach (IXLRow row in ws.RowsUsed())
+                {
+                    rowLocation++;
+                    Debug.WriteLine($"Processing row {rowLocation}");
+
+                    if (rowLocation == 1) { continue; }  // Skip header row.
+
+                    string model = row.Cell(1).GetValue<string>();
+
+                    if (model.Length != 5) { continue; } // Only 5 letter model codes.
+
+                    int qty = GetSalesQuantity(model);
+
+                    ws.Cell(rowLocation, 4).SetValue<int>(qty);
+                }
+
+                rowLocation = 0;
+
+                // 4 letter model codes.
+                foreach (IXLRow row in ws.RowsUsed())
+                {
+                    rowLocation++;
+                    Debug.WriteLine($"Processing row {rowLocation}");
+
+                    if (rowLocation == 1) { continue; }  // Skip header row.
+
+                    string model = row.Cell(1).GetValue<string>();
+
+                    if (model.Length != 4) { continue; } // Only 4 letter model codes.
+
+                    int qty = GetSalesQuantity(model);
+
+                    ws.Cell(rowLocation, 4).SetValue<int>(qty);
+                }
+
+                wb.Save();
+            }
+
+            Process.Start(fileName);
+
+        }
+
+        private static int GetSalesQuantity(string model)
+        {
+            List<string> partNbrs = new List<string>();
+
+
+            switch (model.Length)
+            {
+                case 4:
+                    partNbrs = _ims.Where(i => i.Family4 == model).Select(i => i.Part_Number).Distinct().ToList();
+                    break;
+
+                case 5:
+                    partNbrs = _ims.Where(i => i.Family5 == model).Select(i => i.Part_Number).Distinct().ToList();
+                    break;
+
+                default:
+                    throw new NotSupportedException($"Model length of  '{model.Length}' is not supported.");
+
+            }
+
+            DateTime dateStart = new DateTime(2017, 6, 1);
+            DateTime dateEnd = new DateTime(2018, 5, 31);
+
+            int? quantity = _M2KDb.vSA_Sales.Where(s => partNbrs.Contains(s.Part_Nbr) && s.Inv_So_Date >= dateStart && s.Inv_So_Date <= dateEnd).Sum(s => s.Quantity);
+
+            // Remove part numbers from the IM list, so we don't double-count.
+            _ims.RemoveAll(i => partNbrs.Contains(i.Part_Number));
+
+            if(quantity.HasValue)
+            {
+                return quantity.Value;
+            }
+            else
+            {
+                return 0;
+            }
         }
     }
 }
